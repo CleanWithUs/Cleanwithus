f3316030bf465710c3ee59610388f25b711faef8
CleanWithUs <149815281+CleanWithUs@users.noreply.github.com>
2023-11-03 12:21:05 -0400

    Add files via upload

---
+++ text.js
@@ -0,0 +1,51 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu')
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".gallery-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay:{
+        delay:3000,
+        disableOnInteraction: false,
+    },
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    spaceBetween:20,
+    autoplay:{
+        delay:5000,
+        disableOnInteraction: false,},
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  AOS.init({
+    duration: 800,
+    delay: 400,
+
+  });
